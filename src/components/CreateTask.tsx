afbf45483777f29435233057306ceba672742d7e
ExiledSpirit <gab.gabriel.1002@outlook.com>
2022-12-25 15:38:21 -0300

    Finalizando componente de criar task

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,15 +1,41 @@
 import styles from './CreateTask.module.css';
 import { PlusCircle } from 'phosphor-react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-export function CreateTask() {
+interface CreateTaskProps {
+  addTask: (task: string) => void;
+}
+
+export function CreateTask({addTask}: CreateTaskProps) {
+  const [newTask, setNewTask] = useState('');
+
+  console.log(newTask);
+  
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+    
+    addTask(newTask);
+
+    setNewTask('');
+  }
+
   return (
-    <form className={styles.form}>
+    <form
+      className={styles.form}
+      onSubmit={handleCreateNewTask}
+    >
       <input
         placeholder='Adicione uma nova tarefa'
         autoComplete='false'
         spellCheck='false'
+        value={newTask}
+        onChange={handleNewTaskChange}
       />
-      <button><span>Criar</span><PlusCircle size={16} /></button>
+      <button><span>Criar</span><PlusCircle size={20} /></button>
     </form>
   );
 }
